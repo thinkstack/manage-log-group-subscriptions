0d76e86ca1d2671987ceea04c1a0cc6eb11c3a34
James McNeil <jmcneil@equalexperts.com>
2018-04-11 12:39:42 +0100

    TEL-602: gets log group names and subscription filters

--- log_handler_subscription.py
+++ log_handler_subscription.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python
+import json
 
+#Add exception handling for all IO
+
+def get_lambda_log_group_names(lambda_client):
+    response = lambda_client.describe_log_groups()
+    # retry on token
+    response_json = json.load(response)
+    return list(map(lambda x: x['logGroupName'], response_json['logGroups']))
+
+def get_log_groups_with_subscription_filters(lambda_client):
+    response = lambda_client.describe_subscription_filters()
+    #retry on token
+    response_json = json.load(response)
+    return list(map(lambda x: x['logGroupName'], response_json['subscriptionFilters']))
--- tests/log_handler_subscription.py
+++ tests/log_handler_subscription.py
@@ -1,1 +1,37 @@
 from mock import MagicMock, patch, call
+import log_handler_subscription as log_handler
+import unittest
+import os
+
+
+class TestLambda(unittest.TestCase):
+
+    def setUp(self):
+        self.log_group_name_prefix = '/aws/prefix/'
+        self.log_group_names = [
+            self.log_group_name_prefix + 'group_1',
+            self.log_group_name_prefix + 'group_2',
+            self.log_group_name_prefix + 'group_3'
+        ]
+
+
+    def test_get_lambda_log_groups(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(dir_path + "/test-data/log-group-response.json") as json_data:
+            lambda_client = MagicMock()
+            lambda_client.describe_log_groups.return_value = json_data
+            response = log_handler.get_lambda_log_group_names(lambda_client)
+
+            lambda_client.describe_log_groups.assert_called_once_with()
+            self.assertEqual(response, self.log_group_names)
+
+    def test_get_log_groups_with_subscription_filters(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(dir_path + "/test-data/subscription-filters-response.json") as json_data:
+            lambda_client = MagicMock()
+            lambda_client.describe_subscription_filters.return_value = json_data
+            response = log_handler.get_log_groups_with_subscription_filters(lambda_client)
+
+            lambda_client.describe_subscription_filters.assert_called_once_with()
+            self.assertEqual(response, [self.log_group_name_prefix + 'group_1'])
+
