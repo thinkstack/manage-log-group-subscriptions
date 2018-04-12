29f11586bf7c58391c52298ac5842257ee9e7a79
James McNeil <jmcneil@equalexperts.com>
2018-04-12 12:04:48 +0100

    TEL-602: adds outstanding TODOs

--- log_handler_subscription.py
+++ log_handler_subscription.py
@@ -2,7 +2,7 @@
 import boto3
 import json
 
-#Add exception handling for all IO
+#TODO: Add exception handling for all IO
 
 def lambda_handler(event, context):
     log_client = boto3.client('logs')
@@ -15,13 +15,13 @@ def lambda_handler(event, context):
 
 def get_log_group_names(log_client):
     response = log_client.describe_log_groups()
-    # retry on token
+    #TODO: retry on token
     response_json = json.load(response)
     return set(map(lambda x: x['logGroupName'], response_json['logGroups']))
 
 def get_log_groups_with_subscription_filters(log_client):
     response = log_client.describe_subscription_filters()
-    #retry on token
+    #TODO: retry on token
     response_json = json.load(response)
     return set(map(lambda x: x['logGroupName'], response_json['subscriptionFilters']))
 
