fd501d0f1d4bcf609912583e8b235e99a1db56a1
James McNeil <jmcneil@equalexperts.com>
2018-04-13 12:33:50 +0100

    TEL-602: fixes errors in lambda's use of boto3 api

--- log_handler_subscription.py
+++ log_handler_subscription.py
@@ -6,48 +6,40 @@ import json
 
 
 def lambda_handler(event, context):
-    log_client = boto3.client('logs')
-    lambda_client = boto3.client('lambda')
+    log_client = boto3.client('logs', region_name='eu-west-2')
+    lambda_client = boto3.client('lambda', region_name='eu-west-2')
     log_handler_arn = get_log_handler_arn(lambda_client)
     log_groups = get_log_group_names(log_client)
-    log_groups_with_subscription_filters = get_log_groups_with_subscription_filters(log_client)
-    target_log_groups = log_groups_with_no_subscriptions(log_groups, log_groups_with_subscription_filters)
-    create_subscription_filters(log_client, target_log_groups, log_handler_arn)
+    create_subscription_filters(log_client, log_groups, log_handler_arn)
 
 
 def get_log_group_names(log_client):
     response = log_client.describe_log_groups()
     # TODO: retry on token
-    response_json = json.load(response)
-    return set(map(lambda x: x['logGroupName'], response_json['logGroups']))
-
-
-def get_log_groups_with_subscription_filters(log_client):
-    response = log_client.describe_subscription_filters()
-    # TODO: retry on token
-    response_json = json.load(response)
-    return set(map(lambda x: x['logGroupName'], response_json['subscriptionFilters']))
-
-
-def log_groups_with_no_subscriptions(group_names, groups_with_subscriptions):
-    return group_names - groups_with_subscriptions
+    return set(map(lambda x: x['logGroupName'], response['logGroups']))
 
 
 def get_log_handler_arn(lambda_client):
-    response = lambda_client.get_function(FunctionName='log_handler')
-    response_json = json.load(response)
-    return response_json['Configuration']['FunctionArn']
+    response = lambda_client.get_function_configuration(FunctionName='log_handler')
+    return response['FunctionArn']
 
 
 def create_subscription_filters(log_client, log_group_names, log_handler_arn):
     for log_group_name in log_group_names:
         function_name = get_function_name(log_group_name)
-        log_client.put_subscription_filter(
-            logGroupName=log_group_name,
-            filterName=function_name + '-log-handler-lambda-subscription',
-            filterPattern='',
-            destinationArn=log_handler_arn
-        )
+        try:
+            log_client.put_subscription_filter(
+                logGroupName=log_group_name,
+                filterName=function_name + '-log-handler-lambda-subscription',
+                filterPattern='',
+                destinationArn=log_handler_arn
+            )
+        except log_client.exceptions.LimitExceededException:
+            # subscription filter already exists
+            pass
+        except log_client.exceptions.InvalidParameterException:
+            # attempting to subscribe log_handler logs to log_handler
+            pass
     return
 
 
--- tests/log_handler_subscription.py
+++ tests/log_handler_subscription.py
@@ -2,6 +2,7 @@ from mock import MagicMock, patch, call
 import log_handler_subscription
 import unittest
 import os
+import json
 
 
 class TestLambda(unittest.TestCase):
@@ -20,35 +21,20 @@ class TestLambda(unittest.TestCase):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         with open(dir_path + "/test-data/log-group-response.json") as json_data:
             lambda_client = MagicMock()
-            lambda_client.describe_log_groups.return_value = json_data
+            lambda_client.describe_log_groups.return_value = json.load(json_data)
             response = log_handler_subscription.get_log_group_names(lambda_client)
 
             lambda_client.describe_log_groups.assert_called_once_with()
             self.assertEqual(response, self.log_group_names)
 
-    def test_get_log_groups_with_subscription_filters(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        with open(dir_path + "/test-data/subscription-filters-response.json") as json_data:
-            lambda_client = MagicMock()
-            lambda_client.describe_subscription_filters.return_value = json_data
-            response = log_handler_subscription.get_log_groups_with_subscription_filters(lambda_client)
-
-            lambda_client.describe_subscription_filters.assert_called_once_with()
-            self.assertEqual(response, {self.log_group_name_prefix + 'group_1'})
-
-    def test_groups_with_no_subscriptions(self):
-        result = log_handler_subscription.log_groups_with_no_subscriptions(self.log_group_names, {'/aws/prefix/group_2'})
-
-        self.assertEqual(result, {self.log_group_name_prefix + 'group_1', 'group_3'})
-
     def test_get_log_handler_arn(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         with open(dir_path + "/test-data/log_handler.json") as json_data:
             lambda_client = MagicMock()
-            lambda_client.get_function.return_value = json_data
+            lambda_client.get_function_configuration.return_value = json.load(json_data)
             response = log_handler_subscription.get_log_handler_arn(lambda_client)
 
-            lambda_client.get_function.assert_called_once_with(FunctionName='log_handler')
+            lambda_client.get_function_configuration.assert_called_once_with(FunctionName='log_handler')
             self.assertEqual(response, self.log_handler_arn)
 
     def test_create_subscription_filters(self):
