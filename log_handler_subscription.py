18dc66baf1ff7a6591a5b01a6c5568faaf4b39a9
Lee Myring <29373851+thinkstack@users.noreply.github.com>
2022-07-19 08:41:29 +0100

    TEL-3153: bump python 3.9, update packages, run black
    Co-authored-by: Ricky Shelton <89852508+rickyshelton-madetech@users.noreply.github.com>

--- log_handler_subscription.py
+++ log_handler_subscription.py
@@ -5,8 +5,8 @@ import boto3
 
 
 def lambda_handler(event, context):
-    log_client = boto3.client('logs', region_name='eu-west-2')
-    lambda_client = boto3.client('lambda', region_name='eu-west-2')
+    log_client = boto3.client("logs", region_name="eu-west-2")
+    lambda_client = boto3.client("lambda", region_name="eu-west-2")
     log_handler_arn = get_log_handler_arn(lambda_client)
     log_groups = get_log_group_names(log_client, [], None)
     create_subscription_filters(log_client, log_groups, log_handler_arn)
@@ -14,9 +14,11 @@ def lambda_handler(event, context):
 
 def get_log_group_names(log_client, previous_group_names, next_token):
     response = get_paginated_log_groups(log_client, next_token)
-    group_names = previous_group_names + list(map(lambda x: x['logGroupName'], response['logGroups']))
-    if 'nextToken' in response:
-        return get_log_group_names(log_client, group_names, response['nextToken'])
+    group_names = previous_group_names + list(
+        map(lambda x: x["logGroupName"], response["logGroups"])
+    )
+    if "nextToken" in response:
+        return get_log_group_names(log_client, group_names, response["nextToken"])
     else:
         return group_names
 
@@ -29,12 +31,12 @@ def get_paginated_log_groups(log_client, next_token):
 
 
 def get_log_handler_arn(lambda_client):
-    response = lambda_client.get_function_configuration(FunctionName='log_handler')
-    return response['FunctionArn']
+    response = lambda_client.get_function_configuration(FunctionName="log_handler")
+    return response["FunctionArn"]
 
 
 def create_subscription_filters(log_client, log_group_names, log_handler_arn):
-    excluded_log_group_names = os.getenv('EXCLUDED_LOG_GROUPS', '').split(',')
+    excluded_log_group_names = os.getenv("EXCLUDED_LOG_GROUPS", "").split(",")
     for log_group_name in log_group_names:
         if log_group_name in excluded_log_group_names:
             continue
@@ -42,9 +44,9 @@ def create_subscription_filters(log_client, log_group_names, log_handler_arn):
         try:
             log_client.put_subscription_filter(
                 logGroupName=log_group_name,
-                filterName=function_name + '-log-handler-lambda-subscription',
-                filterPattern='',
-                destinationArn=log_handler_arn
+                filterName=function_name + "-log-handler-lambda-subscription",
+                filterPattern="",
+                destinationArn=log_handler_arn,
             )
         except log_client.exceptions.LimitExceededException:
             # subscription filter already exists
@@ -56,7 +58,7 @@ def create_subscription_filters(log_client, log_group_names, log_handler_arn):
 
 
 def get_function_name(log_group_name):
-    arr = log_group_name.split('/')
+    arr = log_group_name.split("/")
     if len(arr[-1]) == 0:
         return arr[-2]
     else:
